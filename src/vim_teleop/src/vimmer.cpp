7eca3b149a1f416a219f33544d0b0c707c32ecbc
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-06-22 16:24:49 +0000

    implement vimmer node

--- src/vim_teleop/src/vimmer.cpp
+++ src/vim_teleop/src/vimmer.cpp
@@ -2,17 +2,18 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
+using namespace std;
 using namespace std::chrono_literals;
 
 class Publisher : public rclcpp::Node
 {
 public:
   Publisher()
-  : Node("simple_publisher"), count_(0)
+  : Node("imvimmer")
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&Publisher::timer_callback, this));
   }
@@ -20,14 +21,46 @@ public:
 private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+    RCLCPP_INFO(this->get_logger(), "Enter Key(+Enter) as Vimmer");
+    RCLCPP_INFO(this->get_logger(), "j:forward, k:backward, h:left, l:right"); 
+
+    geometry_msgs::msg::Twist vel;
+
+    char key;
+    cin >> key;
+
+    switch(key)
+    {
+      case 'j': /* forward */
+        RCLCPP_INFO(this->get_logger(), "forwarding,,,"); 
+        vel.linear.x = 0.20;
+        break;
+      case 'k': /* backward */
+        RCLCPP_INFO(this->get_logger(), "backwarding,,,"); 
+        vel.linear.x = -0.20;
+        break;
+      case 'h': /* left */
+        RCLCPP_INFO(this->get_logger(), "turning left,,,"); 
+        vel.angular.z = 0.50;
+        break;
+      case 'l': /* right */
+        RCLCPP_INFO(this->get_logger(), "turning right,,,"); 
+        vel.angular.z = -0.50;
+        break;
+      default:  /* boo! */
+        RCLCPP_INFO(this->get_logger(), "Input key is invalid");
+        RCLCPP_INFO(this->get_logger(), "Are you really Vimmer?");
+        vel.linear.x = 0.00;
+        vel.angular.z = 0.00;
+        break;
+    }
+    RCLCPP_INFO(this->get_logger(),
+        "Publishing: linear = %.2lf angular = %.2lf",
+        vel.linear.x, vel.angular.z);
+    publisher_->publish(vel);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
 
 int main(int argc, char * argv[])
