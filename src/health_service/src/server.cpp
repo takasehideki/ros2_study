76c54a04429ad9eaeeab9900864d01abee8e8700
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-06-23 05:00:58 +0000

    re-implement server with class

--- src/health_service/src/server.cpp
+++ src/health_service/src/server.cpp
@@ -3,31 +3,38 @@
 
 #include <memory>
 
-void calc_bmi(const std::shared_ptr<health_msgs::srv::Health::Request> request,
-    std::shared_ptr<health_msgs::srv::Health::Response> response)
+class Server : public rclcpp::Node
 {
-  RCLCPP_INFO(rclcpp::get_logger("health_server"), 
-      "Incoming request.\n height: %u / weight: %.2f", request->height, request->weight);
+public:
+  Server()
+  : Node("health_server")
+  {    
+    auto calc_bmi = [this](            
+      const std::shared_ptr<health_msgs::srv::Health::Request> request,
+      std::shared_ptr<health_msgs::srv::Health::Response> response) -> 
+    void {      
+      RCLCPP_INFO(this->get_logger(), 
+          "Incoming request.\n height: %u / weight: %.2f", 
+          request->height, request->weight);
 
-  response->bmi = request->weight / (request->height / 100.0) / (request->height / 100.0);
+      response->bmi = request->weight / (request->height / 100.0) / (request->height / 100.0);
 
-  RCLCPP_INFO(rclcpp::get_logger("health_server"), 
-      "sending back response.\n %s's BMI is [%.2f]", request->name.c_str(), response->bmi);
-}
+      RCLCPP_INFO(this->get_logger(), 
+          "sending back response.\n %s's BMI is [%.2f]", 
+          request->name.c_str(), response->bmi);
+    };     
+    service_ = this->create_service<health_msgs::srv::Health>(
+        "health_service",calc_bmi);
+    RCLCPP_INFO(this->get_logger(), "Ready to calculate BMI.");
+  } 
+  private:  
+  rclcpp::Service<health_msgs::srv::Health>::SharedPtr service_;
+};
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-
-  std::shared_ptr<rclcpp::Node> node = 
-      rclcpp::Node::make_shared("health_server");
-
-  rclcpp::Service<health_msgs::srv::Health>::SharedPtr service =
-      node->create_service<health_msgs::srv::Health>("health_service",
-      &calc_bmi);
-
-  RCLCPP_INFO(rclcpp::get_logger("health_server"), "Ready to calculate BMI.");
-
-  rclcpp::spin(node);
+  rclcpp::spin(std::make_shared<Server>());
   rclcpp::shutdown();
+  return 0;
 }
