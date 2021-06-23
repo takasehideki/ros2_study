4f8fbddb870f277d5e6aac54722927ede7b4f162
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-06-23 05:32:26 +0000

    add Subscriber of /odom for responding
    change the names and scopes of g_odom_position_{x,y} from odom_position_{x,y}_ due to following compilation error:
    
    /home/ubuntu/ros2_ws/src/velpose_service/src/server.cpp: In member function ‘void Server::odom_callback(nav_msgs::msg::Odometry_<std::allocator<void> >::SharedPtr) const’:
    /home/ubuntu/ros2_ws/src/velpose_service/src/server.cpp:59:22: error: assignment of member ‘Server::odom_position_x_’ in read-only object
       59 |     odom_position_x_ = msg->pose.pose.position.x;
          |     ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~
    /home/ubuntu/ros2_ws/src/velpose_service/src/server.cpp:60:22: error: assignment of member ‘Server::odom_position_y_’ in read-only object
       60 |     odom_position_y_ = msg->pose.pose.position.y;
          |     ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~

--- src/velpose_service/src/server.cpp
+++ src/velpose_service/src/server.cpp
@@ -3,9 +3,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "velpose_msgs/srv/vel_pose.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 #include <chrono>
 using namespace std::chrono_literals;
+
+float_t g_odom_position_x;
+float_t g_odom_position_y;
+
+using std::placeholders::_1;
 
 class Server : public rclcpp::Node
 {
@@ -33,8 +39,8 @@ public:
         loop_rate.sleep();
       }
 
-      response->position_x = odom_position_x_;
-      response->position_y = odom_position_y_;
+      response->position_x = g_odom_position_x;
+      response->position_y = g_odom_position_y;
       RCLCPP_INFO(this->get_logger(), 
           "Position after operating : x %.2f / y %.2f", 
           response->position_x, response->position_y);
@@ -44,16 +50,24 @@ public:
     service_ = this->create_service<velpose_msgs::srv::VelPose>(  
         "velpose_service", vlp_server);
     publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 10, std::bind(&Server::odom_callback, this, _1));
 
     RCLCPP_INFO(this->get_logger(), "Ready to run VelPose server");
   }
 
 private:
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const
+  {
+    g_odom_position_x = msg->pose.pose.position.x;
+    g_odom_position_y = msg->pose.pose.position.y;
+    RCLCPP_DEBUG(this->get_logger(), 
+        "Position after operating : x %.2f / y %.2f", 
+        g_odom_position_x, g_odom_position_y);
+  }
   rclcpp::Service<velpose_msgs::srv::VelPose>::SharedPtr service_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-
-  float_t odom_position_x_;
-  float_t odom_position_y_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
 };
 
 int main(int argc, char **argv)
