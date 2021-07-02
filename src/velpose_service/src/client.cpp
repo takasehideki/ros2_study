642b8ae5100219d76cd8f97684c3ea51f5b00736
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-07-02 13:20:51 +0900

    insert comment to velpose_service

--- src/velpose_service/src/client.cpp
+++ src/velpose_service/src/client.cpp
@@ -1,4 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
+/* 独自に定義したVelPose型のインクルード */
 #include "velpose_msgs/srv/vel_pose.hpp"
 
 #include <chrono>
@@ -11,20 +12,24 @@ int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
+  /* 引数(個数)のチェック */
   if (argc != 3) {
     RCLCPP_INFO(rclcpp::get_logger("vlp_client"), 
         "usage: linear[m/s] angular[rad/s]");
     return 1;
   }
 
+  /* ノードを生成して service client として登録 */
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("vlp_client");
   rclcpp::Client<velpose_msgs::srv::VelPose>::SharedPtr client =
       node->create_client<velpose_msgs::srv::VelPose>("velpose_service");
 
+  /* request として送信する情報を引数から取得 */
   auto request = std::make_shared<velpose_msgs::srv::VelPose::Request>();
   request->linear  = atof(argv[1]);
   request->angular = atof(argv[2]);
 
+  /* linear,angular の値が適正な範囲かをチェック */
   if( request->linear > 0.22 || request->linear < -0.22) {
     RCLCPP_ERROR(rclcpp::get_logger("vlp_client"), 
         "linear %.2f is invalid value", request->linear);
@@ -40,6 +45,7 @@ int main(int argc, char **argv)
       "Request to send: linear %.2lf / angular %.2lf",
       request->linear, request->angular);
 
+  /* service server の立ち上げを1秒ごとにポーリングして確認 */
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
       RCLCPP_ERROR(rclcpp::get_logger("vlp_client"), 
@@ -53,8 +59,10 @@ int main(int argc, char **argv)
   RCLCPP_INFO(rclcpp::get_logger("vlp_client"), 
       "service is now available, sending the request,,,");
 
+  /* server の処理結果を受信して result に格納 */
   auto result = client->async_send_request(request);
 
+  /* server での処理の成否を確認して結果をログ出力 */
   if (rclcpp::spin_until_future_complete(node, result) ==
     rclcpp::FutureReturnCode::SUCCESS)
   {
--- src/velpose_service/src/server.cpp
+++ src/velpose_service/src/server.cpp
@@ -1,21 +1,26 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
+/* 独自に定義したVelPose型のインクルード */
 #include "velpose_msgs/srv/vel_pose.hpp"
+/* Twist型, Odometry型のインクルード */
 #include "geometry_msgs/msg/twist.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 
 #include <chrono>
 using namespace std::chrono_literals;
 
+/* /odom から購読する {x,y} 座標を保持する変数 */
 float_t g_odom_position_x;
 float_t g_odom_position_y;
 
+/* ノードのオブジェクトを操作するためのスマートポインタ */
 using std::placeholders::_1;
 
 class Server : public rclcpp::Node
 {
 public:
+  /* service server の生成時に実行される処理 */
   Server()
   : Node("vlp_server")
   {
@@ -27,18 +32,21 @@ public:
           "Incoming request: linear %.2f / angular %.2f", 
           request->linear, request->angular);
 
+      /* client から受信した request を格納 */
       geometry_msgs::msg::Twist vel;
       vel.linear.x  = request->linear;
       vel.angular.z = request->angular;
   
       RCLCPP_INFO(this->get_logger(), "Publishing velocity to TB3,,,");
 
+      /* linear,angular を /cmd_vel に対して100msごとに出版 */
       rclcpp::WallRate loop_rate(100ms);
       for(int i=0; i<100; i++){
         publisher_->publish(vel);
         loop_rate.sleep();
       }
 
+      /* client に返送する {x,y} 座標を response に格納 */
       response->position_x = g_odom_position_x;
       response->position_y = g_odom_position_y;
       RCLCPP_INFO(this->get_logger(), 
@@ -47,6 +55,7 @@ public:
 
       RCLCPP_INFO(this->get_logger(), "Operation on VelPose server finished");
     };
+    /* service, publisher, subscriber をそれぞれ生成 */
     service_ = this->create_service<velpose_msgs::srv::VelPose>(  
         "velpose_service", vlp_server);
     publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
@@ -57,6 +66,7 @@ public:
   }
 
 private:
+  /* /odom の購読時に実行されるコールバック関数 */
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const
   {
     g_odom_position_x = msg->pose.pose.position.x;
@@ -65,6 +75,7 @@ private:
         "Position after operating : x %.2f / y %.2f", 
         g_odom_position_x, g_odom_position_y);
   }
+  /* service, publisher, subscriber のためのスマートポインタ */
   rclcpp::Service<velpose_msgs::srv::VelPose>::SharedPtr service_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
